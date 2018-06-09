312c92ef0f508b969774c4c2f5a124ec66ead46d
Sonu <sonu.khobragade2@gmail.com>
2018-06-09 12:16:46 +0530

    z

---
+++ Automation/src/test/java/com/name/practice/A.java
@@ -0,0 +1,10 @@
+package com.name.practice;
+
+public abstract class A {
+	// Concrete Methods
+	static void m1() {
+		System.out.println("m1");
+	}
+	public static void main(String[] args) {
+	}
+}
---
+++ Automation/src/test/java/com/name/practice/AbstractClass.java
@@ -0,0 +1,11 @@
+package com.name.practice;
+
+public abstract class AbstractClass {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public abstract void xyz();
+	
+}
